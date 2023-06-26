650e5c39417b387166256cb3819328eccb3ec95b
rpgraffi <rpgmelone@gmail.com>
2023-06-26 17:22:09 +0200

    haha

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Hey
+
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
